a8b115048e4f348901664a80d5db297b47e97973
pen <petter.nordholm@softhouse.se>
2012-04-20 14:47:01 +0200

    * Added guava and initial url decoding.

--- spotify_hd/src/se/softhouse/garden/spotify/webserver/ArtistServlet.java
+++ spotify_hd/src/se/softhouse/garden/spotify/webserver/ArtistServlet.java
@@ -1,6 +1,10 @@
 package se.softhouse.garden.spotify.webserver;
 
 import java.io.IOException;
+import java.net.HttpRetryException;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +15,23 @@ import javax.servlet.http.HttpServletResponse;
 public class ArtistServlet extends HttpServlet 
 {
 
+	Pattern REGEXP = Pattern.compile("^/(.*?)/(.*?)/(.*?)$"); 
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException 
 	{
-		// TODO Auto-generated method stub
+		String uri = req.getPathInfo();
+		Matcher matcher = REGEXP.matcher(uri);
+		if(matcher.matches())
+		{
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		System.out.println("Artist is: " + URLDecoder.decode(matcher.group(1),"UTF-8"));
+		System.out.println("Album is: " + URLDecoder.decode(matcher.group(2),"UTF-8"));
+		System.out.println("Image is: " + URLDecoder.decode(matcher.group(3),"UTF-8"));
 		super.doGet(req, resp);
 	}
 	
