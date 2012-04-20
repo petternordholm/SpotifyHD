5568e251cdcf72393fd0e331148f50df345c3985
Anders Johansson <anders.johansson@softhouse.se>
2012-04-20 14:16:22 +0200

    more

--- spotify_hd/src/se/softhouse/garden/spotify/webserver/Webserver.java
+++ spotify_hd/src/se/softhouse/garden/spotify/webserver/Webserver.java
@@ -1,11 +1,18 @@
 package se.softhouse.garden.spotify.webserver;
 
+import org.eclipse.jetty.server.Server;
+
 public class Webserver {
 
+	private Server server;
 	
 	private void start(){
 		try{
-			
+			server = new Server(9988);
+		    server.setHandler(new ApiHandler());
+		    server.start();
+		    System.out.println("webserver running");
+		    server.join();
 		}catch(Throwable t){
 			
 		}
@@ -13,8 +20,8 @@ public class Webserver {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Webserver ws = new Webserver();
+		ws.start();
 	}
 
 }
