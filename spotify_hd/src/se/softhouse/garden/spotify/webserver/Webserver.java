1b80c3031d038ff6759894ddb37cb923369bda20
Anders Johansson <anders.johansson@softhouse.se>
2012-04-20 14:09:49 +0200

    init2

--- spotify_hd/src/se/softhouse/garden/spotify/webserver/Webserver.java
+++ spotify_hd/src/se/softhouse/garden/spotify/webserver/Webserver.java
@@ -2,9 +2,16 @@ package se.softhouse.garden.spotify.webserver;
 
 public class Webserver {
 
-	/**
-	 * @param args
-	 */
+	
+	private void start(){
+		try{
+			
+		}catch(Throwable t){
+			
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
